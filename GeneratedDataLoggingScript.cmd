8fcce40f9590fdcfca7e3a133b44b62f9816b55a
Megan Mahajan <megan@energyinnovation.org>
2020-03-18 11:37:59 -0700

    Ran export tool and created data logging script for calibration

---
+++ GeneratedDataLoggingScript.cmd
@@ -0,0 +1,9 @@
+SPECIAL>LOADMODEL|"EPS.mdl"
+
+SIMULATE>SAVELIST|OutputVarsToExport-Calibration.lst
+SIMULATE>RUNNAME|NoSettings
+SIMULATE>READCIN|
+MENU>RUN|O
+MENU>VDF2TAB|NoSettings.vdf|NoSettings.tsv|OutputVarsToExport-Calibration.lst|||||:NoSettings
+
+SIMULATE>SAVELIST|
